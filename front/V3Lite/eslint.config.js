5cd875a69e3a06de183f871ae565b649415bb871
rongrui <tangqi1462@gmail.com>
2026-04-24 16:41:54 +0800

    chore: relax vue template lint rules

--- front/V3Lite/eslint.config.js
+++ front/V3Lite/eslint.config.js
@@ -42,7 +42,10 @@ export default tseslint.config(
         },
       ],
       'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      'vue/html-self-closing': 'off',
+      'vue/max-attributes-per-line': 'off',
       'vue/multi-word-component-names': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
     },
   },
   {
